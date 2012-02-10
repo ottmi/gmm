40e8c31f2c3d4f41f2ea1ac92a7956d704baafc5
Michael Ott <michael.ott@csiro.au>
2012-02-10 16:09:58 +1100

    Added missing globals.h to repository

---
+++ globals.h
@@ -0,0 +1,24 @@
+#ifndef GLOBALS_H_
+#define GLOBALS_H_
+#include <string>
+using namespace std;
+
+#define _DNA_DATA				0
+#define	_AA_DATA				1
+#define _DICODON_DATA		2
+
+#define _DNA_MAP      "ACGTURYKMSWBDHVN?-"
+#define _AA_MAP       "ACDEFGHIKLMNPQRSTVWYBJXZ?-"
+
+typedef struct opt_struct
+{
+	string alignment;
+	unsigned int alignmentGrouping;
+	string inputTree;
+	bool help;
+} Options;
+
+extern int verbose;
+extern unsigned int charStates;
+
+#endif /* GLOBALS_H_ */
